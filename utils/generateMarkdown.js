d7473a82f0c489ac68442e298dbe08a5261df5f3
JonWong2020 <jonathannwong50@aol.com>
2022-09-12 15:39:28 -0700

    added functions to generatemarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const fs = require('fs');
 
-// TODO: Create a function that returns the license link
+// returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `[![${license} license](https://img.shields.io/badge/License-${license}-green.svg)](${renderLicenseLink(license)})`
+  }
+}
+
+// returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return `https://lbesson.mit-license.org/`
+  }
+  if (license === 'Apache') {
+    return `https://spdx.org/licenses/Apache-2.0.html`
+  }
+  if (license === 'ISC') {
+    return `https://spdx.org/licenses/ISC.html`
+  }
+  if (license === 'GNU GPLv3') {
+    return `https://www.gnu.org/licenses/gpl-3.0-standalone.html`
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
